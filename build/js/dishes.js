e67764b52be9eda6ffca5eb3ddcf865e090f8198
olendeer <olendeer666@gmail.com>
2021-01-22 12:10:53 +0300

    Added home and stocks adaptive versions

--- build/js/dishes.js
+++ build/js/dishes.js
@@ -4,12 +4,48 @@ window.addEventListener('load', () => {
         slidesToScroll: 3,
         slidesToShow: 4,
         variableWidth: true,
-        infinite: false
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToScroll: 2,
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                }
+            }
+        ]
     })
     const dishesSlider = document.querySelector('.dishes-slider .slick-list')
-    dishesSlider.style.paddingLeft = `${dishesHeader.getBoundingClientRect().left}px`
-    window.addEventListener('resize', () => {
+    if(window.innerWidth > 1280){
         dishesSlider.style.paddingLeft = `${dishesHeader.getBoundingClientRect().left}px`
+    } else if (window.innerWidth > 1024 && window.innerWidth < 1280) {
+        dishesSlider.style.paddingLeft = `${70}px`
+    } else if (window.innerWidth > 768 && window.innerWidth < 1024) {
+        dishesSlider.style.paddingLeft = `${55}px`
+    } else if (window.innerWidth > 480 && window.innerWidth < 768){
+        dishesSlider.style.paddingLeft = `${32}px`
+    } else if (window.innerWidth < 480){
+        dishesSlider.style.paddingLeft = `${30}px`
+    }
+    window.addEventListener('resize', () => {
+        if(window.innerWidth > 1280){
+            dishesSlider.style.paddingLeft = `${dishesHeader.getBoundingClientRect().left}px`
+        } else if (window.innerWidth > 1024 && window.innerWidth < 1280) {
+            dishesSlider.style.paddingLeft = `${70}px`
+        } else if (window.innerWidth > 768 && window.innerWidth < 1024) {
+            dishesSlider.style.paddingLeft = `${55}px`
+        } else if (window.innerWidth > 480 && window.innerWidth < 768){
+            dishesSlider.style.paddingLeft = `${32}px`
+        } else if (window.innerWidth < 480){
+            dishesSlider.style.paddingLeft = `${30}px`
+        }
     })
     document.querySelector('.dishes-wrapper .next').addEventListener('click', () => {
         $('.dishes-slider').slick('slickNext')
--- build/js/index.js
+++ build/js/index.js
@@ -14,10 +14,38 @@ window.addEventListener('load', () => {
         variableWidth: true,
         infinite: false
     })
-    teamSlider.style.marginLeft = `${header.getBoundingClientRect().left}px`
-    teamSlider.style.width = `calc(100vw - ${header.getBoundingClientRect().left}px)`
-    window.addEventListener('resize', () => {
+    if(window.innerWidth > 1280){
         teamSlider.style.marginLeft = `${header.getBoundingClientRect().left}px`
         teamSlider.style.width = `calc(100vw - ${header.getBoundingClientRect().left}px)`
+    } else if(window.innerWidth > 1024 && window.innerWidth < 1280) {
+        teamSlider.style.marginLeft = `70px`
+        teamSlider.style.width = `calc(100vw - 70px)`
+    } else if(window.innerWidth > 768 && window.innerWidth < 1024) {
+        teamSlider.style.marginLeft = `55px`
+        teamSlider.style.width = `calc(100vw - 55px)`
+    } else if (window.innerWidth > 480 && window.innerWidth < 768){
+        teamSlider.style.marginLeft = `32px`
+        teamSlider.style.width = `calc(100vw - 32px)`
+    } else if (window.innerWidth < 480){
+        teamSlider.style.marginLeft = `30px`
+        teamSlider.style.width = `calc(100vw - 30px)`
+    }
+    window.addEventListener('resize', () => {
+        if(window.innerWidth > 1280){
+            teamSlider.style.marginLeft = `${header.getBoundingClientRect().left}px`
+            teamSlider.style.width = `calc(100vw - ${header.getBoundingClientRect().left}px)`
+        } else if(window.innerWidth > 1024 && window.innerWidth < 1280) {
+            teamSlider.style.marginLeft = `70px`
+            teamSlider.style.width = `calc(100vw - 70px)`
+        } else if(window.innerWidth > 768 && window.innerWidth < 1024) {
+            teamSlider.style.marginLeft = `55px`
+            teamSlider.style.width = `calc(100vw - 55px)`
+        } else if (window.innerWidth > 480 && window.innerWidth < 768){
+            teamSlider.style.marginLeft = `32px`
+            teamSlider.style.width = `calc(100vw - 32px)`
+        } else if (window.innerWidth < 480){
+            teamSlider.style.marginLeft = `30px`
+            teamSlider.style.width = `calc(100vw - 30px)`
+        }
     })
 })
